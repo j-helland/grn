ebe1e0f22114eb86103a8b1fce7d44234359c54b
j-helland <jonathan.w.helland@gmail.com>
2021-11-12 16:57:22 -0500

    Making setup.py release-ready.

--- setup.py
+++ setup.py
@@ -9,8 +9,22 @@ install_requires = [
 
 setup(
     name='glb',
-    install_requires=install_requires,
     packages=['glb'],
+    version='0.1',
+    license='unlicense',
+    description='GPU Load Balancer (GLB). Map N jobs to M devices on the same machine.',
+    author='Jonathan Helland',
+    url='https://github.com/j-helland/glb',
+    download_url='https://github.com/j-helland/glb/archive/v_01.tar.gz',
+    keywords=['GPU', 'load balance'],
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Machine Learning :: Training Tools',
+        'License :: OSI Approved :: The Unlicense',
+        'Programming Language :: Python :: 3',
+    ],
     entry_points={
         'console_scripts': [
             'glb-start=glb.scripts.glb_start:start',
